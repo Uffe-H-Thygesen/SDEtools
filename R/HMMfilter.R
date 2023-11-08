57b38ab4cbcdc3957e2f295a3e7f13fa50e4f620
Uffe H Thygesen <uhth@dtu.dk>
2023-11-08 21:18:07 +0100

    Added Viterbi

--- R/HMMfilter.R
+++ R/HMMfilter.R
@@ -8,7 +8,7 @@
 ##' @param D function mapping state (numeric scalar) to diffusivity (numeric scalar)
 ##' @param xi The numerical grid. Numeric vector of increasing values, giving cell boundaries
 ##' @param bc String indicating boundary conditions. See details.
-##' @param x0dist Prior distribution of the initial state, specified as a c.d.f. or as a vector of same length as xi, start
+##' @param x0dist Prior distribution of the initial state. See details. 
 ##' @param tvec Vector of (increasing) time points where observations are taken
 ##' @param yvec Vector of observations at each time point
 ##' @param lfun Likelihood function so that lfun(x,y) gives the likelihood of y given x
@@ -16,11 +16,20 @@
 ##' @param do.Viterbi Do we want the most probable state sequence, found with the Viterbi algorithm?
 ##' @param pfun C.d.f. of observations given states, i.e. pfun(x,y) gives P(Y<=y | X = x). If supplied, pseudo-prediction residuals will be computed
 ##' 
+##' @details 
+##' The distribution of the initial condition x0 can be specified in a number of ways: 
+##'   . If x0dist is a function, it is interpreted as the c.d.f. of the initial state
+##'   . If x0dist is a single number, it is interpreted as a deterministic initial state
+##'   . If x0dist is a numeric vector of the same length as xi, then it is interpreted as a c.d.f.
+##'   . If x0dist is a numeric vector with one less element than xi, then it is interpreted as cell probabilities.
+##'   
 ##' @return A list containing:
 ##' phi A tabulation of the predicitive probability densities
 ##' psi A tabulation of the estimated probability densities
 ##' pi  (If do.smooth==TRUE) A tabulation of the smoothed probability densities
-##' c   A vector containing the normalization constants at each time step 
+##' c   A vector containing the normalization constants at each time step
+##' X A vector containing the most probabile path (if do.Viterbi==TRUE)
+##' U A vector containint the pseudo-prediction residuals (if pfun is supplied)
 ##' 
 ##' @author Uffe Høgsbro Thygesen
 ##'
@@ -30,14 +39,14 @@ HMMfilterSDE <-
 {
     G <- fvade(u,D,xi,bc);
 
-    xc <- head(xi,-1) + 0.5*diff(xi)
+    xc <- cell.centers(xi)
 
     nx <- length(xc)
     nt <- length(tvec)
 
     dt <- diff(tvec)
 
-    if(var(dt)/(mean(dt)^2) < 0.01)
+    if(var(dt)/(mean(dt)^2) < 1e-8)
     {
         dt <- mean(dt)
     } else {
@@ -59,8 +68,9 @@ HMMfilterSDE <-
     }
     if(is.numeric(x0dist))
     {
-        if(length(x0dist)==length(xc)) phi0 <- x0dist        # x0dist contains probabilities of cells
-        if(length(x0dist)==length(xi)) phi0 <- diff(x0dist)  # x0dist contains c.d.f. at cell interfaces
+        if(length(x0dist)==1) phi0 <- diff(xi>x0dist)       # x0dist contains a deterministic IC
+        if(length(x0dist)==nx) phi0 <- x0dist               # x0dist contains probabilities of cells
+        if(length(x0dist)==length(xi)) phi0 <- diff(x0dist) # x0dist contains c.d.f. at cell interfaces
     }
         
     if(is.null(x0dist))
@@ -77,7 +87,7 @@ HMMfilterSDE <-
     ## Transition probabilities 
     P <- as.matrix(Matrix::expm(G*dt))
 
-    for(i in 1:(length(tvec)-1))
+    for(i in 1:(nt-1))
     {
         psi[i,] = phi[i,] * ltab[,i];      # Data update
         c[i] <- sum(psi[i,])
@@ -119,7 +129,32 @@ HMMfilterSDE <-
         ans <- c(ans,list(pi=pi,lambda=lambda,mu=mu))
     }
 
-    if(do.Viterbi) print("The Viterbi algorithm has not been implemented yet.")
+    if(do.Viterbi) {
+        ## Backward dynamic programming iteration
+        ## V is the value function (log-likelihood);
+        ## U is the strategy (next state)
+        U <- V <- array(NA,c(nx,nt))
+
+        lP <- log(P)
+        
+        V[,nt] <- log(ltab[,nt])
+        for(t in nt:2)
+        {
+            W <- outer(log(ltab[,t-1]),rep(1,nx)) + lP + outer(rep(1,nx),V[,t])
+            V[,t-1] <- apply(W,1,max)
+            U[,t-1] <- apply(W,1,which.max)
+        }
+
+        ## Forward loop, finding the path
+        X <- numeric(nt)
+        print(V[,1])
+        
+        X[1] <- which.max(V[,1])
+        for(t in 2:nt) X[t] <- U[X[t-1],t-1]
+
+        ans <- c(ans,list(X=X))
+    }
+
 
     if(!is.null(pfun))
     {
@@ -131,7 +166,6 @@ HMMfilterSDE <-
 
         ans <- c(ans,list(U=U))
     }
-
 
     return(ans)
 }
